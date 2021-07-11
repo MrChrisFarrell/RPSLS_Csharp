c87b2113b5b6ef7a9dd751cadf3da7e135ca7c2c
unknown <mrchrisfarrell@yahoo.com>
2021-07-10 23:45:57 -0400

    choice and rock classes added

---
+++ RPSLS_Csharp/Choice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPSLS_Csharp
+{
+    public class Choice
+    {
+        public string name;
+        public bool win;
+
+        public virtual void Compare(string name)
+        {
+
+        }
+    }
+}
---
+++ RPSLS_Csharp/Rock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPSLS_Csharp
+{
+    public class Rock : Choice
+    {
+        public Rock()
+        {
+            this.name = "Rock";
+        }
+
+        public override void Compare(string name)
+        {
+            if(name == "Scissors" || name == "Lizard")
+            {
+                this.win = true;
+            }
+            else
+            {
+                this.win = false;
+            }
+        }
+    }
+}
